ee824475faa919649d2915269b6d3b87472daa9c
korem7 <koray3019@gmail.com>
2017-12-17 23:42:05 +0300

    add unit test code

--- observer/tests.py
+++ observer/tests.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+import re
 # Create your tests here.
+
+class TwitterClientTests(TestCase):
+
+	def is_clean_tweet(self):
+		test_string = 'this is a real life example of special characters \ \r \t'
+		result = clean_tweet(self, test_string)
+		if re.match("^[a-zA-Z0-9_]*$", result):
+			self.assertTrue(True)
+		else:
+			self.assertFalse(True)
+
